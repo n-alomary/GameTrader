e319663d079573692deb1a7048e1a9b22d19590c
Nour Alomary <nour.alomary@myfuturesbright.com>
2015-07-23 17:24:28 +0100

    yolo

---
+++ GameTrader/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameTrader.MvcApplication" Language="C#" %>
---
+++ GameTrader/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameTrader.Startup))]
+namespace GameTrader
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
